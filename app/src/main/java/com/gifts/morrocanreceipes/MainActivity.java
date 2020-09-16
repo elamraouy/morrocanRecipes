6bf0d20e058e95fca22a9262d025b939b19f1512
elamraouy <69376769+elamraouy@users.noreply.github.com>
2020-09-16 20:46:36 +0100

    new commit

---
+++ app/src/main/java/com/gifts/morrocanreceipes/MainActivity.java
@@ -0,0 +1,27 @@
+package com.gifts.morrocanreceipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    // create app menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_items, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // toolbar
+        Toolbar toolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(toolbar);
+    }
+}
